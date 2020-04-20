2e91ed7b81fbb303970415dfc03a8d346e654856
Milutin Gavrilovic <milutingavrilovic@protonmail.com>
2020-04-21 00:33:03 +0200

    update latest

--- src/components/common/Charts/styles.js
+++ src/components/common/Charts/styles.js
@@ -3,11 +3,11 @@ import { makeStyles } from "@material-ui/styles";
 export default makeStyles((theme) => ({
   charts: {
     position: 'relative',
-    height: '100%',
     display: 'flex',
     flexDirection: 'column',
     flexGrow: 1,
-    width: '100%'
+    width: '100%',
+   height: '100%'
   },
   container: {
     color: 'white',
@@ -16,6 +16,7 @@ export default makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
     maxWidth: 1200,
+    height: 'calc(100% - 25px)'
   },
   chatsContainer: {
     display: 'flex',
@@ -25,10 +26,11 @@ export default makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
     margin: 5,
-    padding: 5,
+    // padding: 5,
     border: '1px solid #363636',
     backgroundColor: '#222222',
-    zIndex: 9999
+    zIndex: 9999,
+    height: 'calc(100% - 10px)'
   },
   typography: {
     lineHeight: 2,
