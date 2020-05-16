eeadfd6cc57a7e1ab038d5bed1ba178d0c05057c
ryientsoft-dell <you@example.com>
2020-05-17 01:12:20 +0530

    Collpase table for level 3 and $

---
+++ src/components/common/LevelsNestedTreeGrid/CollapseLevel2.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+// import useRowStyles from './styles';
+// import CollapsibleTable from './CollapsibleTable'
+
+const useRowStyles = makeStyles({
+    root: {
+      '& > *': {
+        borderBottom: 'unset',
+      },
+    },
+  });
+  
+  
+function Row(props) {
+  const { row } = props;
+  console.log(props.row,'ashikah')
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+  return (
+    <React.Fragment>
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>
+          {row ? row.id : ''}
+        </TableCell>
+        <TableCell>{row ? row.name : ''}</TableCell>
+        <TableCell>{row ? row.level : ''}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Table size="small" aria-label="purchases">
+                <TableBody>
+                  <CollapsibleTable row={row}/>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+function RowWithoutCollapse(props) {
+    const { row } = props;
+    console.log(props.row,'ashikah')
+    const [open, setOpen] = React.useState(false);
+    const classes = useRowStyles();
+    return (
+      <React.Fragment>
+        <TableRow className={classes.root}>
+          <TableCell>
+            <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </TableCell>
+          <TableCell>
+            {row ? row.id : ''}
+          </TableCell>
+          <TableCell>{row ? row.name : ''}</TableCell>
+          <TableCell>{row ? row.level : ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box margin={1}>
+                <Table size="small" aria-label="purchases">
+                  <TableBody>
+                   <TableRow >
+                      <TableCell component="th" scope="row">
+                        ID level 4
+                      </TableCell>
+                      <TableCell>Name level 4</TableCell>
+                      <TableCell align="right">Level 4</TableCell>
+                     
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    );
+  }
+
+export default function CollapsibleTable(props) {
+  const row = props.data
+  return (
+    <TableContainer >
+      <Table aria-label="collapsible table">
+      <TableRow>
+            <TableCell />
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Level</TableCell>
+        </TableRow>
+        <TableBody>
+            {
+                row 
+                ?
+                <Row key={row.name} row={row} /> 
+                :
+                <RowWithoutCollapse />
+            }
+            
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
